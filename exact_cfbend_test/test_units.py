a15627878854ca6d9246ecd41d3a523c00e88eb7
Eric G. Stern <egstern@fnal.gov>
2025-08-21 11:36:12 -0500

    test_units.py show thats entering the units for ExactCFbend object in unnormalized form the k multipole moments get normalized and retained in the object data members in that form.

---
+++ exact_cfbend_test/test_units.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import sys, os
+import numpy as np
+import impactx
+
+from scipy.constants import c, eV, m_p
+
+mp = 1.0e-9*m_p*c**2/eV
+print('proton mass: ', mp, "GeV")
+
+KE = 0.8
+
+def main():
+    E = mp + KE
+    gamma = E/mp
+    betagamma = np.sqrt(gamma**2 - 1)
+    beta = betagamma/gamma
+
+    brho = c*1.0e-9
+    print('brho: ', brho)
+
+    # make 4 m long magnet with 15 degree CFbend
+    L = 4
+    theta = np.pi/12
+    # BL/Brho = theta
+    #
+    # The k_0 coefficient is the normalized magnetic field or
+    # B/Brho = 1/rho
+    
+    # rho is L/theta
+
+    one_over_R = theta/L
+
+    kn = np.array([one_over_R, 0.0, 0.0])
+    ks = np.array([0.0, 0.0, 0.0])
+
+    cfb_obj1 = impactx.elements.ExactCFbend(ds=L,
+                                            k_normal=kn,
+                                            k_skew=ks,
+                                            nslice=4,
+                                            name='foo')
+
+    print('obj1 contains: ', dir(cfb_obj1))
+    print('obj1 dict: ', cfb_obj1.to_dict())
+
+    # what does it look like if I use unnormalized units?
+    p = mp*betagamma
+    pnorm = p/brho
+
+    B = pnorm * one_over_R
+    print('B: ', B, 'T')
+    knn = np.array([B, 0.0, 0.0])
+
+    cfb_obj2 = impactx.elements.ExactCFbend(ds=L,
+                                            k_normal=kn,
+                                            k_skew=ks,
+                                            unit=1,
+                                            nslice=4,
+                                            name='foo')
+
+    print('obj2 dict: ', cfb_obj2.to_dict())
+
+if __name__ == "__main__":
+    main()
+    pass
