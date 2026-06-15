dfe61146e2dfe5594157e89573df0feccf6582e9
Eric G. Stern <egstern@fnal.gov>
2026-06-15 15:39:00 -0500

    * Add MPI support * Write out run progress energy, rf voltage, freq, phase * Add hook for setting RF parameters turn-by-turn * Use Options class for run settings

--- booster-accel/booster_accel.py
+++ booster-accel/booster_accel.py
@@ -16,6 +16,7 @@ from booster_rf import *
 pip2ramp = PIP2ramp()
 
 from booster_momentum import *
+
 from booster_set_rf import set_rf
 
 from impactx import ImpactX, distribution, elements, twiss, synmadx
@@ -23,11 +24,23 @@ from impactx import ImpactX, distribution, elements, twiss, synmadx
 from booster_accel_options import Options
 from scipy.constants import c
 
+from mpi4py import MPI
+myrank = MPI.COMM_WORLD.rank
+
 #========================================================================
+
+opts = Options()
+
+# write out RF and energy status turn-by-turn on rank 0
+if myrank == 0:
+    runstatus = open('runstatus.txt', 'w')
+    print("turn time gamma V phase", file=runstatus)
+    pass
 
 # Update RF cavities for next turn
 def update_rf_cavities_next_turn(sim):
     ref = sim.beam.ref
+    booster_momentum = Booster_momentum(ref)
     current_beta = ref.beta
     current_gamma = ref.gamma
     above_transition = current_gamma > opts.gamma_tr
@@ -36,7 +49,7 @@ def update_rf_cavities_next_turn(sim):
     current_time = ref.t/c
     lattice_length = sum(elem.ds for elem in sim.lattice)
     delta_t = lattice_length/(current_beta*c)
-    E_next_turn = e_vs_t(current_time + delta_t)
+    E_next_turn = booster_momentum.e_vs_t(current_time + delta_t)
     new_freq = opts.harmonic_number * current_beta * c/lattice_length
     required_energy_gain = E_next_turn - current_E
     current_V = pip2ramp.get_rf_voltage_by_time(current_time)
@@ -46,7 +59,11 @@ def update_rf_cavities_next_turn(sim):
         phase_needed = np.pi/2
     else:
         phase_needed = np.arcsin(required_energy_gain/current_V)
-    set_rf(sim, current_V, freq=new_freq, phase=phase_needed, above_transition=above_transition)
+    set_rf(sim, current_V, freq=new_freq, phaseR=phase_needed, above_transition=above_transition)
+
+    print(sim.tracking_period, current_time, current_gamma, current_V,
+          phase_needed, file=runstatus, flush=True)
+
     return
 
 
@@ -86,16 +103,21 @@ def main():
     sim.beam.set_bucket_length(bucket_length)
 
     # element to read in particles
-    particle_source = elements.Source(openpmd_path=opts.particles_file, active_once=True, name="particles")
-    monitor = elements.Beammonitor("monitor.h5", backup="h5")
+    particle_source = elements.Source(distribution="openPMD", openpmd_path=opts.particles_file, active_once=True, name="particles")
+    monitor = elements.BeamMonitor("monitor.h5", backend="h5")
 
     sim.lattice.clear()
     sim.lattice.append(particle_source)
     sim.lattice.extend(ix_lattice)
     sim.lattice.append(monitor)
 
+    # Set up for updating RF and frequency
+    sim.hook["before_period"] = update_rf_cavities_next_turn
 
-
+    sim.periods = opts.turns
+    sim.verbose = 0
+    sim.track_particles()
+    
     sim.finalize()
 
     return
